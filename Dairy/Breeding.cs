8243408b06d57e4139e14544363df5789e8079ba
EslamElkurdi <elkurdieslam@gmail.com>
2023-02-27 20:52:30 +0200

    Breeding Form

---
+++ Dairy/Breeding.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dairy
+{
+    public partial class Breeding : Form
+    {
+        public Breeding()
+        {
+            InitializeComponent();
+        }
+
+        private void bunifuMaterialTextbox4_OnValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
