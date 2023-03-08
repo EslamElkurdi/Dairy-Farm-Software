b66002f2a9f2d870098c88528ce06a0ec1af3f54
EslamElkurdi <elkurdieslam@gmail.com>
2023-03-08 08:10:40 +0200

    Fill Cow Id Function in Health Form

--- Dairy/Cows Health.cs
+++ Dairy/Cows Health.cs
@@ -148,6 +148,19 @@ namespace Dairy
 
         SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Eslam\OneDrive\Documents\DairyFarmDb.mdf;Integrated Security=True;Connect Timeout=30");
 
+        private void FillCowId()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select CowId from CowsTbl");
+            SqlDataReader Rdr;
+            Rdr = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Columns.Add("CowId", typeof(int));
+            dt.Load(Rdr);
+            CowIdCb.ValueMember = "CowId";
+            CowIdCb.DataSource = dt;
+            con.Close();
+        }
     }
 
 
