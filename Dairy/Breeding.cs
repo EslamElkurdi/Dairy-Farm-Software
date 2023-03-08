25519f3cb115b5d13ba4564e482b6a75c3f693a5
EslamElkurdi <elkurdieslam@gmail.com>
2023-03-08 09:41:34 +0200

    Get Cow Name Function is Created

--- Dairy/Breeding.cs
+++ Dairy/Breeding.cs
@@ -102,6 +102,21 @@ namespace Dairy
             con.Close();
         }
 
+        private void GetCowName()
+        {
+            con.Open();
+            String query = "select * from CowTbl where CowId=" + CowIdCb.SelectedValue.ToString() + "";
+            SqlCommand cmd = new SqlCommand(query, con);
+            DataTable dt = new DataTable();
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            sda.Fill(dt);
+            foreach (DataRow dr in dt.Rows)
+            {
+                CowNameTb.Text = dr["CowName"].ToString();
+            }
+
+            con.Close();
+        }
         private void button3_Click(object sender, EventArgs e)
         {
             
