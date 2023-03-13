d3ae332419c7c66e2ff3f008c0b26eccc552843e
EslamElkurdi <elkurdieslam@gmail.com>
2023-03-13 15:12:11 +0200

    connection in Employee form

--- Dairy/Employee.cs
+++ Dairy/Employee.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -21,6 +22,10 @@ namespace Dairy
         {
 
         }
+
+
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Eslam\OneDrive\Documents\DairyFarmDb.mdf;Integrated Security=True;Connect Timeout=30");
+
 
         private void guna2HtmlLabel8_Click(object sender, EventArgs e)
         {
