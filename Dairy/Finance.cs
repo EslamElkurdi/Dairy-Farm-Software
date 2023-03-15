2048d65f15494a49d9a074f6e8ef1d8a0694530a
EslamElkurdi <elkurdieslam@gmail.com>
2023-03-15 10:30:13 +0200

    Clear function

--- Dairy/Finance.cs
+++ Dairy/Finance.cs
@@ -93,7 +93,11 @@ namespace Dairy
             con.Close();
         }
 
-       
+        private void clearExp()
+        {
+            Purpose.SelectedIndex = -1;
+            Amount.Text = "";
+        }
 
 
 
